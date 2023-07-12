6958c90eb0858f7aa475c5d88f5aacccccc93459
whiskacz <pawelblasiak1985@gmail.com>
2023-07-12 22:59:02 +0200

    React Practice R10

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from 'react-dom/client'
+import { BsSearch } from 'react-icons/bs'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+document.body.style = {
+    margin: '0',
+    padding: '0',
+    boxSizing: 'border-box'
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const wrapperStyling ={
+    backgroundColor: '#555',
+    height: "100vh",
+    width: '100vw',
+   
+
+}
+
+
+const loupStyling = {
+    position: "absolute",
+    top: "5%",
+    right: '5%',
+    fontSize: "50px",
+}
+const searchBarStyling = {
+    position: 'absolute',
+    top: '30%'
+}
+
+
+function R10App(){
+    return (
+        <div style={wrapperStyling}>
+        <input type="text" style={{searchBarStyling}}
+        />
+        <BsSearch style={loupStyling}/>
+        </div>
+    )
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<R10App />)
